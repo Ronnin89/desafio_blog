a174bd436881af615f3f34f4a7c6272382a82a65
Ricardo Alvarez <ri.alvarez.89@icloud.com>
2021-02-09 13:15:45 -0300

    authenticate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+    http_basic_authenticate_with name: 'desafiovamoscontodo', password: 'XAHTJEAS23123%23', except: :index
+
     def index
         @posts= Post.all.order(created_at: :desc)
     end
